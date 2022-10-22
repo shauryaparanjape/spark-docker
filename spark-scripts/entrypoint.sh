29bde79d757e148c094c0629e0f98340292400e1
Shourya Paranjape <sparanjape@Shouryas-MBP.ht.home>
2022-10-22 14:15:41 -0400

    initial commit all files

---
+++ spark-scripts/entrypoint.sh
@@ -0,0 +1,3 @@
+start-master.sh
+start-worker.sh spark://py-spark:7077
+tail -f /dev/null
---
+++ spark-scripts/spark-env.sh
@@ -0,0 +1,3 @@
+export PYSPARK_PYTHON="/usr/bin/python3"
+export PYSPARK_DRIVER_PYTHON="/usr/bin/python3"
+export SPARK_MASTER_HOST="py-spark"
